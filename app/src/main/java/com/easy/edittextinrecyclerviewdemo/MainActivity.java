958dbf06c252a6af607c070b885927df6133f7e2
顾叶菁 <gu.yj@ikcrm.com>
2017-11-24 14:44:38 +0800

    fix

--- app/src/main/java/com/easy/edittextinrecyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/easy/edittextinrecyclerviewdemo/MainActivity.java
@@ -7,9 +7,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
@@ -19,23 +19,23 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
+    private LinearLayoutManager llm;
+    private RecyclerView.Adapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         recyclerView = (RecyclerView) findViewById(R.id.rv);
-
-        recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.setLayoutManager(llm=new LinearLayoutManager(MainActivity.this, LinearLayoutManager.VERTICAL, false));
         recyclerView.addItemDecoration(new DividerItemDecoration(MainActivity.this,DividerItemDecoration.VERTICAL_LIST));
-        recyclerView.setItemViewCacheSize(0);
-        recyclerView.setAdapter(new RecyclerView.Adapter() {
-
+        recyclerView.setAdapter(adapter=new RecyclerView.Adapter() {
             //输入法
             InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
             //edittext里的文字内容集合
             SparseArray<String> etTextAry = new SparseArray();
+            //edittext的焦点位置
+            int etFocusPos = -1;
             TextWatcher textWatcher = new TextWatcher() {
                 @Override
                 public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -47,11 +47,11 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void afterTextChanged(Editable s) {
+                    //每次修改文字后，保存在数据集合中
+                    Log.e("tag","index="+etFocusPos+",save="+s.toString());
                     etTextAry.put(etFocusPos, s.toString());
                 }
             };
-            //edittext的焦点位置
-            int etFocusPos = -1;
 
             @Override
             public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -61,65 +61,60 @@ public class MainActivity extends AppCompatActivity {
             }
 
             @Override
-            public void onViewRecycled(RecyclerView.ViewHolder holder) {
-                super.onViewRecycled(holder);
-                /**
-                 * 当前holder被销毁时，把holder的TextChangedListener删除
-                 */
-                ItemHolder viewHolder = (ItemHolder) holder;
-                viewHolder.et.removeTextChangedListener(textWatcher);
-            }
-
-            @Override
-            public void onBindViewHolder(RecyclerView.ViewHolder holder, int i) {
-
+            public synchronized void onBindViewHolder(RecyclerView.ViewHolder holder, int i) {
+                Log.e("tag","绑定Holder,index="+i);
                 final int position = i;
-                final ItemHolder viewHolder = (ItemHolder) holder;
-
+                ItemHolder viewHolder = (ItemHolder) holder;
                 viewHolder.tv.setText("item "+position);
                 viewHolder.et.setText(etTextAry.get(position));
-                if (etFocusPos == position) {
-                    viewHolder.et.requestFocus();
-                    viewHolder.et.setSelection(viewHolder.et.getText().length());
-                }
-                viewHolder.et.addTextChangedListener(textWatcher);
-
-                viewHolder.et.setOnTouchListener(new View.OnTouchListener() {
+                viewHolder.et.setOnFocusChangeListener(new View.OnFocusChangeListener() {
                     @Override
-                    public boolean onTouch(View v, MotionEvent event) {
-                        if (event.getAction() == MotionEvent.ACTION_UP) {
+                    public void onFocusChange(View view, boolean b) {
+                        if (b){
                             etFocusPos = position;
+                            Log.e("tag","etFocusPos焦点选中-"+etFocusPos);
                         }
-                        return false;
                     }
                 });
             }
 
             @Override
             public int getItemCount() {
-                return 40;
+                return 50;
             }
 
             @Override
             public void onViewDetachedFromWindow(RecyclerView.ViewHolder holder) {
                 super.onViewDetachedFromWindow(holder);
+                Log.e("tag","隐藏item="+holder.getAdapterPosition());
+                ItemHolder viewHolder = (ItemHolder) holder;
+                viewHolder.et.removeTextChangedListener(textWatcher);
+                viewHolder.et.clearFocus();
                 if (etFocusPos == holder.getAdapterPosition()) {
                     inputMethodManager.hideSoftInputFromWindow(((ItemHolder) holder).et.getWindowToken(), 0);
                 }
             }
 
-            class ItemHolder extends RecyclerView.ViewHolder {
+            @Override
+            public void onViewAttachedToWindow(RecyclerView.ViewHolder holder) {
+                super.onViewAttachedToWindow(holder);
+                Log.e("tag","显示item="+holder.getAdapterPosition());
+                ItemHolder viewHolder = (ItemHolder) holder;
+                viewHolder.et.addTextChangedListener(textWatcher);
+                if (etFocusPos == holder.getAdapterPosition()) {
+                    viewHolder.et.requestFocus();
+                    viewHolder.et.setSelection(viewHolder.et.getText().length());
+                }
+            }
 
+            class ItemHolder extends RecyclerView.ViewHolder {
                 private TextView tv;
                 private EditText et;
-
                 public ItemHolder(View itemView) {
                     super(itemView);
                     tv = (TextView) itemView.findViewById(R.id.tv);
                     et = (EditText) itemView.findViewById(R.id.et);
                 }
-
-
             }
         });
     }
